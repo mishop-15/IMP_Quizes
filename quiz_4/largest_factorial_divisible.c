81c9ec49201306f2859e62e6ff3b36df8339fc1a
mishop-15 <153624608+mishop-15@users.noreply.github.com>
2024-05-08 23:32:54 +0530

    Update largest_factorial_divisible.c

--- quiz_4/largest_factorial_divisible.c
+++ quiz_4/largest_factorial_divisible.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-    int factorial(int n){
-    int fac=1;
-    for(int i=1; i<=n; i++){
-        fac= fac*i;
-    }
-    return fac;
-}
-    int main() {
-    int n, result;
-    scanf("%d", &n);
-    for(int i=0; i<20; i++){
-    if(n%factorial(i)==0){
-    result=i;
+
+int largest_factorial(int n) {
+    int factorial = 1;
+    int largest_factorial = 1;
+    int i = 1;
+    while (factorial <= n) {
+        if (n % factorial == 0) {
+            largest_factorial = factorial;
         }
+        i++;
+        factorial *= i;
     }
- printf("%d", result);
-    }
+    return largest_factorial;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int result = largest_factorial(n);
+    printf("%d\n", result);
+    return 0;
+}
