59bfdd82ac675801b95c860a001b6de943581765
mishop-15 <153624608+mishop-15@users.noreply.github.com>
2024-05-08 23:53:06 +0530

    Update matrix_counter_clock.cpp

--- matrix_counter_clock.cpp
+++ matrix_counter_clock.cpp
@@ -3,38 +3,36 @@
 using namespace std;
 
 int main() {
- int n;
- int m;
- cin>>n>>m;
-int arr[n][m];
-for(int i=0;i<n;i++){
-  for(int j=0;j<m;j++){
-    cin>>arr[i][j];
-  }
-}
-int rs=0,re=n-1,cs=0,ce=m-1;
-while(rs<=re && cs<=ce)
-{
-for(int row=rs;row<=re;row++){
-  cout<<arr[row][cs]<<" ";
-}
-cs++;
-for(int col=cs;col<=ce;col++){
-  cout<<arr[re][col]<<" ";
-}
-re--;
-if(cs<=ce){
-for(int row=re;row>=rs;row--){
-  cout<<arr[row][ce]<<" ";
-}
-ce--;
-}
-if(rs<=re){
-for(int col=ce;col>=cs;col--){
-  cout<<arr[rs][col]<<" ";
-}
-rs++;
-}
-
-}
+    int n, m;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    int rs = 0, re = n - 1, cs = 0, ce = m - 1;
+    while (rs <= re && cs <= ce) {
+        for (int row = rs; row <= re; row++) {
+            cout << arr[row][cs] << " ";
+        }
+        cs++;
+        for (int col = cs; col <= ce; col++) {
+            cout << arr[re][col] << " ";
+        }
+        re--;
+        if (cs <= ce) {
+            for (int row = re; row >= rs; row--) {
+                cout << arr[row][ce] << " ";
+            }
+            ce--;
+        }
+        if (rs <= re) {
+            for (int col = ce; col >= cs; col--) {
+                cout << arr[rs][col] << " ";
+            }
+            rs++;
+        }
+    }
+    return 0;
 }
