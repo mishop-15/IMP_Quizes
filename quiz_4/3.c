593bf10f84de43f846d22d8ea65aea1e50e8d8da
mishop-15 <153624608+mishop-15@users.noreply.github.com>
2024-05-08 23:09:59 +0530

    Update and rename 3.cpp to 3.c

---
+++ quiz_4/3.c
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main() {
+  string ans;
+  getline(cin,ans);
+  char c;
+  cin>>c;
+  int first=-1;
+  int last=-1;
+  for(int i=0;i<ans.length();i++){
+if(ans[i]==c){
+if(first==-1){
+  first=i;
+}
+last=i;
+
+  }
+  }
+  cout<<first+1<<last+1;
+    return 0;
+}
